f02c90b959f49182ab80720ae2e9dfe8342d6f1e
rahvavv <rahvavv@mail.ru>
2024-08-15 20:54:33 +0700

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) < 2:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+
+result = get_multiplied_digits(8)
+print(result)
