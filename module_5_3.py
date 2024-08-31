23eca690fe07c44249b66b30578e6a8f4d077bfa
rahvavv <rahvavv@mail.ru>
2024-08-31 23:15:40 +0700

    Update module_5_3.py
    добавил проверку на соответствие

--- module_5_3.py
+++ module_5_3.py
@@ -17,22 +17,46 @@ class House:
         return f"Название: {self.name} кол-во этажей: {self.number_of_floors}"
 
     def __eq__(self, other):
-        return self.number_of_floors == other
+        if isinstance(other.number_of_floors, int) and isinstance(self.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors < other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors > other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors <= other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other
+        if isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            pass
+        else:
+            print("Значение должно быть целым")
+        return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
         print("__add__")
@@ -67,20 +91,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2)  # __eq__
+print(h1 == h2)   # __eq__
 
-h1 = h1 + 10  # __add__
+h1 = h1 + 10      # __add__
 print(h1)
-print(h1 == h2)
+print(h1 == h2)   # __eq__
 
-h1 += 10  # __iadd__
+h1 += 10          # __iadd__
 print(h1)
 
-h2 = 10 + h2  # __radd__
+h2 = 10 + h2      # __radd__
 print(h2)
 
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(h1 > h2)    # __gt__
+print(h1 >= h2)   # __ge__
+print(h1 < h2)    # __lt__
+print(h1 <= h2)   # __le__
+print(h1 != h2)   # __ne__
